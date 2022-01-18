d1f6756c0b2105a6b0f0b835dd3c15e742c5f339
atulsarpate <87242684+atulsarpate@users.noreply.github.com>
2022-01-18 12:19:23 +0530

    Update settings.py

--- first_project/settings.py
+++ first_project/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
+# STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
 
 
 # # Activate Django-Heroku.
